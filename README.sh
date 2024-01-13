2c55bfc67ab5eb7ffdc83eb94290964742492701
Rayna M Harris <raynamharris@users.noreply.github.com>
2024-01-13 07:20:04 -0800

    more white space

--- README.sh
+++ README.sh
@@ -1,12 +1,13 @@
 # read me with links to all files
 
-echo "# Yoga Illustration" > README.md
+echo "# Yoga Illustrations" > README.md
 echo " "  >> README.md
 
 for image in *png
 do
   ls $image
-  echo "Image name: $image  " >> README.md
-  echo "![]($image)  " >> README.md
-  echo "   "  >> README.md
+  echo "![]($image)   " >> README.md
+  echo "Image name: $image   " >> README.md
+  echo "    "  >> README.md
+  echo "    "  >> README.md
 done
